0897e121d32aa98ebc5a905b25c422ca3265c586
robi3377 <robert.uibar@gmail.com>
2023-06-08 22:58:54 +0300

    doamne ajuta

---
+++ cnb/core/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.index),
+    path('catedre', views.catedre),
+    path('contact', views.contact),
+    path('informatii', views.informatii),
+    path('istoric', views.istoric),
+    path('organizarea_claselor', views.organizarea_claselor),
+    path('activitati_extrascolare', views.activitati_extrascolare),
+    path('proiecte', views.proiecte),
+    path('incarcare_proiect', views.incarcare_proiecte),
+    path('template_noutati', views.template_noutati),
+    path('transferuri', views.transferuri),
+    path('database', views.database),
+    path('anunturi', views.anunturi),
+    path('olimpici', views.olimpici),
+    path('programari', views.programari),
+    path('secretariat', views.administrativ),
+    path('proiecte/<str:pk>', views.template_proiecte),
+    path('test', views.test),
+    path('404', views.not_found),
+
+]
